7b3cb471fcce9aa284067aab8e48e67f6de7b695
Jani Niemelä <jani@swappie.com>
2022-11-08 15:59:35 +0200

    Deploy cronjobs from CI

---
+++ deploy_cronjobs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+SHA=$1
+
+if [ -z "$SHA" ]; then
+  echo "Usage: $0 [GIT SHA]"
+  exit 1
+fi
+
+huuto=$(flyctl machines list -a retro-star-scheduled | grep huuto-net-scraper | cut -d$'\t' -f1)
+
+if [ -z "$huuto" ]; then
+  echo "Could not find huuto scraper"
+  exit 1
+fi
+
+fly machines update "$huuto" -a retro-star-scheduled --schedule hourly --image "registry.fly.io/retro-star:$SHA"
+
