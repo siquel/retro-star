6943b1ff94138a328d8238c8ddba5ccb838e72f5
Jani Niemelä <jani@swappie.com>
2022-11-09 20:06:12 +0200

    Implement platform classifier

---
+++ src/common/platform/classifier.ts
@@ -0,0 +1,58 @@
+export enum Platform {
+  NES = 'NES',
+  SNES = 'SNES',
+  Nintendo64 = 'Nintendo64',
+  NintendoGamecube = 'NintendoGamecube',
+  NintendoWii = 'NintendoWii',
+  NintendoWiiU = 'NintendoWiiU',
+  Unknown = 'Unknown',
+}
+
+const NES_RE = /(\bNintendo 8-?bit\b|\bNES\b|\bNintendo Entertaiment System\b)/i
+const SNES_RE = /(\bSNES\b|\bSuper Nintendo\b)/i
+const N64_RE = /(\bN64\b|\bNintendo\b \b64\b)/i
+const GC_RE = /(\bNGC\b|\bGamecube\b)/i
+// Note: negative lookahead match wii not followed by \s*U
+const WII_RE = /\bwii(?!u|\s*u\b)/i
+const WII_U_RE = /\bwii\s*u\b/i
+
+const PLATFORM_REGEX_BY_PLATFORM: ReadonlyMap<Platform, RegExp> = new Map<Platform, RegExp>([
+  [Platform.NES, NES_RE],
+  [Platform.SNES, SNES_RE],
+  [Platform.Nintendo64, N64_RE],
+  [Platform.NintendoGamecube, GC_RE],
+  [Platform.NintendoWii, WII_RE],
+  [Platform.NintendoWiiU, WII_U_RE],
+])
+
+const composeRegexWithout = (platforms: Platform[]) => {
+  const platformsAndRegexes = Array.from(PLATFORM_REGEX_BY_PLATFORM.entries()).filter(([p]) => !platforms.includes(p))
+  if (platformsAndRegexes.length === 0) throw new Error('No platforms available to compose regex')
+
+  return new RegExp(platformsAndRegexes.map(([, re]) => re.source).join('|'), 'gi')
+}
+
+const stripSymbols = (rawString: string) => {
+  return rawString.replace(/[`~!@#$%^&*()_|+=?;:<>{}\[\]\\\/]/gi, '')
+}
+
+export const tryExtractPlatform = (rawString: string) => {
+  const sanitized = stripSymbols(rawString)
+
+  const parsePlatform = (sanitizedStr: string) => {
+    const [platform] = Array.from(PLATFORM_REGEX_BY_PLATFORM).find(([, re]) => re.test(sanitizedStr)) ?? [
+      Platform.Unknown,
+      null,
+    ]
+    return platform
+  }
+
+  const platform = parsePlatform(sanitized)
+
+  // If the source string contains multiple platforms, treat it as unknown for now.
+  if (platform !== Platform.Unknown && composeRegexWithout([platform]).test(sanitized)) {
+    return Platform.Unknown
+  }
+
+  return platform
+}
---
+++ src/common/platform/classifier.unit.test.ts
@@ -0,0 +1,34 @@
+import { Platform, tryExtractPlatform } from './classifier'
+
+describe('Classifier', () => {
+  it.each`
+    source                                                           | expected
+    ${"Nintendo 64 : Yoshi's Story - N64"}                           | ${Platform.Nintendo64}
+    ${'Gradius NES'}                                                 | ${Platform.NES}
+    ${'World Cup 98 Nintendo 64'}                                    | ${Platform.Nintendo64}
+    ${'Carmageddon Nintendo 64 N64'}                                 | ${Platform.Nintendo64}
+    ${'DuckTales nes EEC-SCN'}                                       | ${Platform.NES}
+    ${"Zelda - A link to the past Player's Guide"}                   | ${Platform.Unknown}
+    ${'Nintendo 8-bit - Street fighter 2010 the final fight, USA'}   | ${Platform.NES}
+    ${'Nintendo NES 8bit Knight Rider (L) USA'}                      | ${Platform.NES}
+    ${'Faxanadu SCN (Boxed)'}                                        | ${Platform.Unknown}
+    ${'Batman: Return of the Joker SCN'}                             | ${Platform.Unknown}
+    ${'Resident evil umbrella chronicles wii pal!'}                  | ${Platform.NintendoWii}
+    ${'Fast Racing Neo Wii U'}                                       | ${Platform.NintendoWiiU}
+    ${'Fire Emblem Path of Radiance ja Radiant Dawn Wii / Gamecube'} | ${Platform.Unknown}
+    ${'NES SNES NGC'}                                                | ${Platform.Unknown}
+    ${'Nintendo Gamecube peli Super Mario Sunshine NGC'}             | ${Platform.NintendoGamecube}
+    ${'NGC Worms Blast cib'}                                         | ${Platform.NintendoGamecube}
+    ${'Shrek 2 Nintendo Gamecube'}                                   | ${Platform.NintendoGamecube}
+    ${'Resident Evil 3 Gamecube'}                                    | ${Platform.NintendoGamecube}
+    ${'Stunt race. FX super nintendo'}                               | ${Platform.SNES}
+    ${'wii uber racing'}                                             | ${Platform.NintendoWii}
+    ${'wiiu foobar'}                                                 | ${Platform.NintendoWiiU}
+    ${'wiiusa not a platform'}                                       | ${Platform.Unknown}
+    ${'wii usa is a platform'}                                       | ${Platform.NintendoWii}
+    ${'[NGC] Frogger Beyond'}                                        | ${Platform.NintendoGamecube}
+    ${'Castlevania 3 NES!'}                                          | ${Platform.NES}
+  `('extracts platform from $source to be $expected', ({ source, expected }) => {
+    expect(tryExtractPlatform(source)).toBe(expected)
+  })
+})
